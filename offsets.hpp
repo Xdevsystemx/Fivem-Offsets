b2ca265e333c8f54ca8de679502f2b65e268ebe6
Xdevsystemx <snackslayer25@gmail.com>
2026-08-04 18:16:22 -0500

    Add files via upload

---
+++ offsets.hpp
@@ -0,0 +1,34 @@
+#pragma once
+#include <cstdint>
+
+namespace offsets {
+    constexpr std::ptrdiff_t m_World = 0x25B14B0;
+    constexpr std::ptrdiff_t m_ReplayInterFace = 0x1FBD4F0;
+    constexpr std::ptrdiff_t m_ViewPort = 0x201DBA0;
+    constexpr std::ptrdiff_t m_BlipList = 0x2023400;
+    constexpr std::ptrdiff_t m_CamGameplayDirector = 0x201ED50;
+    constexpr std::ptrdiff_t m_ArmsKinematics = 0x138441C;
+    constexpr std::ptrdiff_t m_LegsKinematics = 0x1385368;
+    constexpr std::ptrdiff_t m_GiveWeapon = 0x101E374;
+    constexpr std::ptrdiff_t m_SilentAim = 0x1038886;
+    constexpr std::ptrdiff_t m_InfiniteAmmo0 = 0x106957C;
+    constexpr std::ptrdiff_t m_InfiniteAmmo1 = 0x10695C1;
+    constexpr std::ptrdiff_t m_MagicBulletsPatch = 0x104EA3E;
+    constexpr std::ptrdiff_t m_InfiniteCombatRoll = 0xB26061;
+    constexpr std::ptrdiff_t m_OffsetPool = 0x1198558;
+    constexpr std::ptrdiff_t m_Speed = 0x70FFAF;
+    constexpr std::ptrdiff_t m_VehicleEngineHealth = 0xF3F808;
+    constexpr std::ptrdiff_t m_Spread = 0x6EF59F;
+    constexpr std::ptrdiff_t m_Recoil = 0x262280;
+    constexpr std::ptrdiff_t m_CObject = 0x3BB209;
+    constexpr std::ptrdiff_t m_CWeapon = 0x77869B;
+    constexpr std::ptrdiff_t m_VehicleDriver = 0xF349A0;
+    constexpr std::ptrdiff_t m_NoRagDoll = 0x60E48C;
+    constexpr std::ptrdiff_t m_SeatBealt = 0x60E456;
+    constexpr std::ptrdiff_t m_VehicleDoorsLockState = 0x8CEA71;
+    constexpr std::ptrdiff_t m_Handling = 0xFED018;
+    constexpr std::ptrdiff_t bullet = 0x101A660;
+    constexpr std::ptrdiff_t aim_cped = 0x202C8D0;
+    constexpr std::ptrdiff_t set_ped_in_to_vehicle = 0x7C3E88;
+    constexpr std::ptrdiff_t c_sky_settings = 0x2721250;
+}
